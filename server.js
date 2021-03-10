efda136b540b6077253ba4a647aab0ab0b878030
EKaxada <enocksdha@gmail.com>
2021-03-10 09:47:15 +0300

    initial commit with server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json()); // use of json data
+
+app.use("/api/products", productRoutes);
+
+require("mongoose")
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on ${process.env.PORT}`)
+);
